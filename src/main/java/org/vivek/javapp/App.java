e566afbdd1d57e8edf84ec137493a43248e7d04a
vivekmahendra <vivekmprac@gmail.com>
2020-11-20 16:26:16 +0530

    java app

---
+++ src/main/java/org/vivek/javapp/App.java
@@ -0,0 +1,13 @@
+package org.vivek.javapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
